acd909b83d7edbe43f3a9b4560d86acdf1afe75a
WoongBeeYang <asdz1214@naver.com>
2022-09-30 17:14:52 +0900

    index 페이지 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Layout from "../components/layout";
 import Image from "next/image";
-import myPhoto from "../image/myPhoto.jpeg"
+import myPhoto from "../image/myPhoto.jpeg";
 
 export default function Home() {
   return (
@@ -13,21 +13,28 @@ export default function Home() {
 
       <section className="text-gray-600 body-font">
         <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
+          <div className="lg:max-w-lg lg:w-full md:w-1/2 mx-auto mb-10 md:mb-0">
             <Image
               className="object-cover object-center rounded"
               alt="hero"
               src={myPhoto}
             />
           </div>
-          <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+          <div className="lg:flex-grow md:w-1/2 lg:pl-18 md:pl-10 flex flex-col md:items-start md:text-left items-center text-center">
             <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-              개발자 하고싶다
+              멋있는 제목 추천좀
             </h1>
-            <p className="mb-8 leading-relaxed">
-              개발자가 되고싶다
+            <p className="mb-8 leading-relaxed break-all">
+              제목에 어울리는 간단한 글 추천좀 씨이발...
             </p>
-            
+            <div className="flex justify-center">
+              <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+                자기소개
+              </button>
+              <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
+                포트폴리오
+              </button>
+            </div>
           </div>
         </div>
       </section>
