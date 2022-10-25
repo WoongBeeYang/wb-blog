57466cad16a70751de49076dbd6478e0bff0b6fe
WoongBeeYang <asdz1214@naver.com>
2022-10-25 16:53:33 +0900

    react-notion-x 불러오기 성공

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,8 @@
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
+import 'react-notion-x/src/styles.css'
+import 'prismjs/themes/prism-tomorrow.css'
+import 'katex/dist/katex.min.css'
 
 function MyApp({ Component, pageProps }) {
   return (
--- pages/introduce.tsx
+++ pages/introduce.tsx
@@ -2,22 +2,112 @@ import Layout from "../components/layout";
 import Head from "next/head";
 import { TOKEN, INTRODUCE } from "../config";
 import { GetlistResult } from "../TypeScript/listType";
+import { NotionAPI } from "notion-client";
+import { NotionRenderer } from "react-notion-x";
+import Image from "next/image";
+import Link from "next/link";
+import dynamic from "next/dynamic";
+import TweetEmbed from "react-tweet-embed";
+import { ExtendedRecordMap } from "notion-types";
+import { useRouter } from "next/router";
+import { getPageTitle } from "notion-utils";
 
-export default function introduce({ data }) {
-  const page_block: GetlistResult = data.results;
-  const introduce_file = page_block[8].file.file.url;
+const Code = dynamic(() =>
+  import("react-notion-x/build/third-party/code").then(async (m) => {
+    // additional prism syntaxes
+    await Promise.all([
+      import("prismjs/components/prism-markup-templating.js"),
+      import("prismjs/components/prism-markup.js"),
+      import("prismjs/components/prism-bash.js"),
+      import("prismjs/components/prism-c.js"),
+      import("prismjs/components/prism-cpp.js"),
+      import("prismjs/components/prism-csharp.js"),
+      import("prismjs/components/prism-docker.js"),
+      import("prismjs/components/prism-java.js"),
+      import("prismjs/components/prism-js-templates.js"),
+      import("prismjs/components/prism-coffeescript.js"),
+      import("prismjs/components/prism-diff.js"),
+      import("prismjs/components/prism-git.js"),
+      import("prismjs/components/prism-go.js"),
+      import("prismjs/components/prism-graphql.js"),
+      import("prismjs/components/prism-handlebars.js"),
+      import("prismjs/components/prism-less.js"),
+      import("prismjs/components/prism-makefile.js"),
+      import("prismjs/components/prism-markdown.js"),
+      import("prismjs/components/prism-objectivec.js"),
+      import("prismjs/components/prism-ocaml.js"),
+      import("prismjs/components/prism-python.js"),
+      import("prismjs/components/prism-reason.js"),
+      import("prismjs/components/prism-rust.js"),
+      import("prismjs/components/prism-sass.js"),
+      import("prismjs/components/prism-scss.js"),
+      import("prismjs/components/prism-solidity.js"),
+      import("prismjs/components/prism-sql.js"),
+      import("prismjs/components/prism-stylus.js"),
+      import("prismjs/components/prism-swift.js"),
+      import("prismjs/components/prism-wasm.js"),
+      import("prismjs/components/prism-yaml.js"),
+    ]);
+    return m.Code;
+  })
+);
+const Collection = dynamic(() =>
+  import("react-notion-x/build/third-party/collection").then(
+    (m) => m.Collection
+  )
+);
+const Equation = dynamic(() =>
+  import("react-notion-x/build/third-party/equation").then((m) => m.Equation)
+);
+const Pdf = dynamic(
+  () => import("react-notion-x/build/third-party/pdf").then((m) => m.Pdf),
+  {
+    ssr: false,
+  }
+);
+const Modal = dynamic(
+  () => import("react-notion-x/build/third-party/modal").then((m) => m.Modal),
+  {
+    ssr: false,
+  }
+);
 
-  
-  const resolution = [];
+const Tweet = ({ id }: { id: string }) => {
+  return <TweetEmbed tweetId={id} />;
+};
 
-  //맘에 안들어서 수정할 방법 생각해야함.
-  for (let i = 2; i < 7; i++) {
-    resolution[i] =
-      page_block[i].callout.icon.emoji +
-      "    " +
-      page_block[i].callout.rich_text[0].plain_text;
+export default function introduce({
+  recordMap,
+  previewImagesEnabled,
+  rootPageId,
+  rootDomain,
+}: {
+  recordMap: ExtendedRecordMap;
+  previewImagesEnabled?: boolean;
+  rootPageId?: string;
+  rootDomain?: string;
+}) {
+  const router = useRouter();
+
+  if (!recordMap) {
+    return null;
   }
 
+  const title = getPageTitle(recordMap);
+  console.log(title, recordMap);
+
+  // useful for debugging from the dev console
+  if (typeof window !== "undefined") {
+    const keys = Object.keys(recordMap?.block || {});
+    const block = recordMap?.block?.[keys[0]]?.value;
+    const g = window as any;
+    g.recordMap = recordMap;
+    g.block = block;
+  }
+
+  const socialDescription = "React Notion X Demo";
+  const socialImage =
+    "https://react-notion-x-demo.transitivebullsh.it/social.jpg";
   return (
     <>
       <Layout>
@@ -25,50 +115,25 @@ export default function introduce({ data }) {
           <title>자기소개</title>
           <meta name="description" content="웅비의 블로그" />
         </Head>
-        <div className="my-10">
-          <div className="flex mx-auto p-5 border md:w-[1000px] w-auto">
-            <div className="p-3 ml-5">
-              {/* 이력서 파일 */}
-              <div className="flex my-3">
-                <div
-                  className="flex flex-col hover:cursor-pointer gap-3"
-                  onClick={() => window.open(`${introduce_file}`)}
-                >
-                  <div className="">
-                    <p className="text-2xl">이력서</p>
-                  </div>
-                  <div className="flex gap-3">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M11.363 2c4.155 0 2.637 6 2.637 6s6-1.65 6 2.457v11.543h-16v-20h7.363zm.826-2h-10.189v24h20v-14.386c0-2.391-6.648-9.614-9.811-9.614zm4.811 13h-2.628v3.686h.907v-1.472h1.49v-.732h-1.49v-.698h1.721v-.784zm-4.9 0h-1.599v3.686h1.599c.537 0 .961-.181 1.262-.535.555-.658.587-2.034-.062-2.692-.298-.3-.712-.459-1.2-.459zm-.692.783h.496c.473 0 .802.173.915.644.064.267.077.679-.021.948-.128.351-.381.528-.754.528h-.637v-2.12zm-2.74-.783h-1.668v3.686h.907v-1.277h.761c.619 0 1.064-.277 1.224-.763.095-.291.095-.597 0-.885-.16-.484-.606-.761-1.224-.761zm-.761.732h.546c.235 0 .467.028.576.228.067.123.067.366 0 .489-.109.199-.341.227-.576.227h-.546v-.944z" />
-                    </svg>
-                    <p className="text-xl">이력서 보기!</p>
-                  </div>
-                </div>
-              </div>
-
-              {/* 각오 */}
-              <div className="">
-                <p className="text-2xl">
-                  {page_block[1].heading_2.rich_text[0].text.content}
-                </p>
-              </div>
-              <div className="flex flex-col">
-                {resolution.map((item, index) => (
-                  <div
-                    key={index}
-                    className="my-1 flex p-4 text-lg bg-[#d4d4d2] dark:bg-[#252525]"
-                  >
-                    <p>{item}</p>
-                  </div>
-                ))}
-              </div>
-            </div>
-          </div>
+        <div className="dark:text-white">
+          <NotionRenderer
+            recordMap={recordMap}
+            fullPage={true}
+            darkMode={true}
+            rootDomain={rootDomain}
+            rootPageId={rootPageId}
+            previewImages={previewImagesEnabled}
+            components={{
+              nextImage: Image,
+              nextLink: Link,
+              Code,
+              Collection,
+              Equation,
+              Pdf,
+              Modal,
+              Tweet,
+            }}
+          />
         </div>
       </Layout>
     </>
@@ -76,21 +141,11 @@ export default function introduce({ data }) {
 }
 
 export async function getStaticProps({}) {
-  const blcokOptions = {
-    method: "GET",
-    headers: {
-      accept: "application/json",
-      "Notion-Version": "2022-06-28",
-      authorization: `Bearer ${TOKEN}`,
-    },
-  };
-  const sibar = await fetch(
-    `https://api.notion.com/v1/blocks/${INTRODUCE}/children?page_size=100`,
-    blcokOptions
-  );
-  const data = await sibar.json();
+  const notion = new NotionAPI();
+
+  const recordMap = await notion.getPage(`${INTRODUCE}`);
 
   return {
-    props: { data },
+    props: { recordMap },
   };
 }
